85d24ff2fe08a254c56b487af89b0d694f5619ee
Satya Prakash Sharma <168863213+withsatya00@users.noreply.github.com>
2025-09-17 13:26:37 +0530

    main.py

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ input_dict = {
 # Button to make prediction
 if st.button('Predict'):
     prediction = predict(input_dict)
-    st.success(f'Predicted Health Insurance Cost: ₹ {prediction} Per Year')
+    st.success(f'Predicted Health Insurance Cost: ₹ {prediction}/Year')
